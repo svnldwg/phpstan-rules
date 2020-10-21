18099ba42b7c9b2f27d1adaf167c737dbebf19b0
Sven Ludwig <s.ludwig@simplicity.ag>
2020-10-21 19:11:50 +0200

    adds support for property annotations

--- src/Rules/ImmutableObjectRule.php
+++ src/Rules/ImmutableObjectRule.php
@@ -44,12 +44,21 @@ class ImmutableObjectRule implements Rule
         
         $nodes = $this->parser->parseFile($scope->getFile());
         
+        $immutableProperties = null;
         if (!$this->classHasWhitelistedAnnotation($nodes)) {
-            return [];
+            $immutableProperties = $this->propertiesWithWhitelistedAnnotations($nodes);
+            
+            if (empty($immutableProperties)) {
+                return [];
+            }
         }
         
         while ($node->var instanceof Node\Expr\ArrayDimFetch) {
             $node = $node->var;
+        }
+        
+        if (!empty($immutableProperties) && !in_array((string)$node->var->name, $immutableProperties)) {
+            return [];
         }
         
         if (
@@ -66,7 +75,8 @@ class ImmutableObjectRule implements Rule
         if ($scope->getFunction() && $scope->getFunction()->isPrivate()) {
             return [
                 RuleErrorBuilder::message(sprintf(
-                    'Class is declared immutable, but class property "%s" is modified in private method "%s" which could be called from outside the constructor',
+                    '%s is declared immutable, but class property "%s" is modified in private method "%s" which could be called from outside the constructor',
+                    empty($immutableProperties) ? 'Class' : 'Property',
                     $node->var->name,
                     $scope->getFunctionName()
                 ))->build()
@@ -75,7 +85,8 @@ class ImmutableObjectRule implements Rule
                         
         return [
             RuleErrorBuilder::message(sprintf(
-                'Class is declared immutable, but class property "%s" is modified in method "%s"',
+                '%s is declared immutable, but class property "%s" is modified in method "%s"',
+                empty($immutableProperties) ? 'Class' : 'Property',
                 $node->var->name,
                 $scope->getFunctionName()
             ))->build()
@@ -103,6 +114,7 @@ class ImmutableObjectRule implements Rule
                         return true;
                     }
                 }
+                continue;
             }
             
             if ($node instanceof Node\Stmt\Class_) {
@@ -114,6 +126,46 @@ class ImmutableObjectRule implements Rule
         }
         
         return false;
+    }
+
+    private function propertiesWithWhitelistedAnnotations(array $nodes): array
+    {
+        $whitelistedProperties = [];
+        
+        foreach ($nodes as $node) {
+            if ($node instanceof Node\Stmt\Namespace_ || $node instanceof Node\Stmt\Declare_) {
+                $subNodeNames = $node->getSubNodeNames();
+                foreach ($subNodeNames as $subNodeName) {
+                    $subNode = $node->{$subNodeName};
+                    if (!is_array($subNode)) {
+                        $subNode = [$subNode];
+                    }
+
+                    $result = $this->propertiesWithWhitelistedAnnotations($subNode);
+                    if (!empty($result)) {
+                        return $result;
+                    }
+                }
+                continue;
+            }
+
+            if ($node instanceof Node\Stmt\Class_) {
+                foreach ($node->stmts as $property) {
+                    if(!$property instanceof Node\Stmt\Property) {
+                        continue;
+                    }
+                    
+                    $whitelisted = $this->isWhitelisted($property);
+                    if ($whitelisted) {
+                        foreach($property->props as $prop) {
+                            $whitelistedProperties[] = (string)$prop->name;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $whitelistedProperties;
     }
     
     private function isWhitelisted(Node $node): bool
